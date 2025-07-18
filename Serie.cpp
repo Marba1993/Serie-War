842d81b878e145d66f492b51c5dcee4b3fbe89f1
Marcos Antonio Banach <104182602+Marba1993@users.noreply.github.com>
2025-07-18 03:54:11 -0300

    Inicial Commit

---
+++ Serie.cpp
@@ -0,0 +1,49 @@
+#include "Serie.h"
+#include <iostream>
+
+using namespace std;
+
+struct Serie
+{
+    string name;
+    int score;
+};
+
+// Arma una serie nueva
+Serie* makeSerie(string name)
+{
+    Serie* newS = new Serie;
+
+    newS->name = name;
+    newS->score = 0;
+    
+
+    return newS; 
+}
+
+// devuelve el nombre de una serie
+string name(Serie* s)
+{
+    return s->name;
+}
+
+
+// devuelve el puntaje total de una serie
+int score(Serie* s)
+{
+    return s->score;
+}
+
+// muestra la serie en consola
+void showSerie(Serie* s)
+{
+    cout << "Serie " << endl;
+    cout << ".: Nombre: " << name(s) << endl;
+    cout << ".: Puntaje: " << score(s) << endl;
+}
+
+// agrega un score a una serie
+void receiveScore(int sc, Serie* s)
+{
+    s->score = s->score + sc ;
+}
---
+++ Serie.h
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+struct Serie;
+
+
+Serie* makeSerie(string name);
+string name(Serie* s);
+int score(Serie* s);
+void showSerie(Serie* s);
+void receiveScore(int score, Serie* s);
